dc7b136f4d06652ab7a5ca6cda9ec724df6fccb5
Gian Tricarico <gtricarico@stuy.edu>
2017-10-31 11:50:49 -0400

    start hw

---
+++ work08.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/stat.h>
+
+int main()
+{
+  struct stat sb;
+  stat("README.md", &sb);
+
+  printf("README.md:\n\tfile size: %d bytes\n\tmode: %o\n\ttime of last access: %s\n", sb.st_size, sb.st_mode, ctime(&sb.st_atime));
+
+  return 0;
+}
