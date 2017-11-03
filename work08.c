dbe618c5e3d2ef174a84623c30d33b8fbc1f6739
Gian Tricarico <gtricarico@stuy.edu>
2017-11-03 00:30:08 -0400

    add makefile and fix warning

--- work08.c
+++ work08.c
@@ -8,7 +8,7 @@ int main()
   struct stat sb;
   stat("README.md", &sb);
 
-  printf("README.md:\n\tfile size: %d bytes\n\tmode: %o\n\ttime of last access: %s\n", sb.st_size, sb.st_mode, ctime(&sb.st_atime));
+  printf("README.md:\n\tfile size: %lu bytes\n\tmode: %o\n\ttime of last access: %s\n", sb.st_size, sb.st_mode, ctime(&sb.st_atime));
 
   return 0;
 }
